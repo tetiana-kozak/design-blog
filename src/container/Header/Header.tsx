1bdd9de907df9b18fdc7733b702101d4836e940e
Tetiana <kozaktanya077@gmail.com>
2023-01-31 22:46:46 +0200

    add basic structure to header

--- src/container/Header/Header.tsx
+++ src/container/Header/Header.tsx
@@ -1,5 +1,25 @@
+import { Container } from '@mui/material'
+import Input from '@mui/material/Input'
+import logo from 'assets/logo-icon.svg'
+
 type Props = {}
 const Header = (props: Props) => {
-  return <div>Header</div>
+  return (
+    <header className="header">
+      <Container>
+        <nav className="navigation">
+          <ul>
+            <li>Blog</li>
+            <li>About Us</li>
+          </ul>
+        </nav>
+        <div className="logo">
+          <div>ArchPress</div>
+          <img src={logo} alt="" />
+        </div>
+        <Input type="search" />
+      </Container>
+    </header>
+  )
 }
 export default Header
