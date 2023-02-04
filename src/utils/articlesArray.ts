9fb4fb5eb939a1e87ded52d6179de653f2333f03
Tetiana <kozaktanya077@gmail.com>
2023-02-04 19:50:08 +0200

    add array with articles

---
+++ src/utils/articlesArray.ts
@@ -0,0 +1,81 @@
+type Articles = {
+  title: string
+  description: string
+  category: string
+}
+
+const ArticlesArray: Articles[] = [
+  {
+    title:
+      'You Can Now Customize Samsung Bespoke Refrigerators With Any Image (*For a Price)',
+    description:
+      'Samsung has unlocked unlimited possibilities by allowing customers to upload any image, design, or photo to adorn their Bespoke fridges.',
+    category: 'technology',
+  },
+  {
+    title:
+      "A Lighting Collection Inspired by Frank Lloyd Wright's Iconic Architecture",
+    description:
+      "The Frank Lloyd Wright series for YAMAGIWA includes representations of a few of the legendary architect's original masterpieces in the form of lighting.",
+    category: 'home furnishing',
+  },
+  {
+    title:
+      'Our Place Opens Melrose Retail Spot With Colors That Reflect Their Pans',
+    description:
+      'Our Place hired Ringo Studio to create a store with visually delightful product displays and a color palette that matches their popular pans.',
+    category: 'commercial',
+  },
+  {
+    title: 'F5: Melanie Courbet Shares the Spark Behind Her Gallery + More',
+    description:
+      "Gallery founder + director Melanie Courbet shares a worthwhile organization in Sierra Leone, the place she just can't stop shopping + more in Friday Five.",
+    category: 'art',
+  },
+  {
+    title: 'Relevo Rug Sculpts a Welcoming Geometric Landscape Underfoot',
+    description:
+      "Light and shadow come to play across Milan-based Studiopepe's Relevo, a new hand-tufted, sculptural wool rug design for Muuto.",
+    category: 'home furnishing',
+  },
+  {
+    title: 'Lee Broom Exhibits Within the "British Capsule" at Maison&Objet',
+    description:
+      'For Maison+Objet\'s "British Capsule," Lee Broom includes a new collection of lighting, as well a selection of complementary furniture and decor.',
+    category: 'home furnishing',
+  },
+  {
+    title: 'The Design Milk X Modenus Talks Lounge Returns to KBIS 2023',
+    description:
+      "The Design Milk X Modenus Talks Lounge is back at KBIS with a schedule that's jam-packed full of presentations + conversations covering design business, social media + trends.",
+    category: 'featured',
+  },
+  {
+    title:
+      'Hotel Riomar Ibiza Celebrates Orange + Neutrals Inspired by Its Surroundings',
+    description:
+      'Hotel Riomar is a boutique hotel in Ibiza that embraces its mid-century layered interiors by celebrating the nostalgic qualities of its past.',
+    category: 'travel',
+  },
+  {
+    title:
+      'An Abandoned Lisbon Attic Becomes Airy Apartment With Hidden Kitchen',
+    description:
+      'KEMA studio transformed the unlivable MARVILA ATTIC into a bright and airy apartment with a hidden kitchen and built-in wall sofa.',
+    category: 'architecture',
+  },
+  {
+    title: 'Desktop Wallpaper: February 2023',
+    description:
+      "Design Milk's founder Jaime Derringer collaborates with Eskayel on a new mural wallpaper + fabric collection – download one for your desktop wallpaper!",
+    category: 'art',
+  },
+  {
+    title: "Vipp's New Pedal Bin Is Rubbish – Literally!",
+    description:
+      "The Rubbish special edition of their iconic pedal bin design is made from plastic leftovers and sawdust from Vipp's own production waste.",
+    category: 'home furnishing',
+  },
+]
+
+export default ArticlesArray
