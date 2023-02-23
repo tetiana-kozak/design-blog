da1793931dc0f8fb109001244d6113670ef159a7
Tetiana <kozaktanya077@gmail.com>
2023-02-23 22:01:03 +0200

    add: basic footer

--- src/container/App/App.tsx
+++ src/container/App/App.tsx
@@ -2,6 +2,7 @@ import Header from '../Header/Header'
 import Main from 'container/Main/Main'
 import CssBaseline from '@mui/material/CssBaseline'
 import { StyledEngineProvider } from '@mui/material/styles'
+import Footer from 'container/Footer/Footer'
 
 type Props = {}
 const App = (props: Props) => {
@@ -9,6 +10,7 @@ const App = (props: Props) => {
     <StyledEngineProvider injectFirst>
       <Header />
       <Main />
+      <Footer />
     </StyledEngineProvider>
   )
 }
---
+++ src/container/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+import { Container } from '@mui/material'
+import './Footer.scss'
+
+type Props = {}
+const Footer = (props: Props) => {
+  return (
+    <footer className="footer">
+      <Container>Footer</Container>
+    </footer>
+  )
+}
+export default Footer
