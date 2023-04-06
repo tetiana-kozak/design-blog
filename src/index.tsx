8cfe19eaf99d8cb963b579e9936a56ee3e023260
Tetiana <kozaktanya077@gmail.com>
2023-04-06 22:05:14 +0300

    add: redux and configure store

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,16 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './container/App/App'
 import './reset.css'
 import './style.scss'
+import { Provider } from 'react-redux'
+import { store } from 'redux/store'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 )
