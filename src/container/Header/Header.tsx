e2c6bdce021b2998d274e384387f7dc2754efab6
Tetiana <kozaktanya077@gmail.com>
2023-04-08 22:01:30 +0300

    add: liked articles counter in header

--- src/container/Header/Header.tsx
+++ src/container/Header/Header.tsx
@@ -4,9 +4,16 @@ import Logo from 'components/Logo/Logo'
 import Search from 'components/Search/Search'
 import './Header.scss'
 import FavoriteIcon from '@mui/icons-material/Favorite'
+import { useAppSelector } from 'redux/hooks'
 
 type Props = {}
 const Header = (props: Props) => {
+  const favoritesState = useAppSelector((state) => state.favoriteArticles)
+
+  const favoriteArticlesCount = Object.values(favoritesState).filter(
+    (value) => value === true
+  ).length
+
   return (
     <header className="main-header">
       <Container>
@@ -15,7 +22,10 @@ const Header = (props: Props) => {
           <Logo />
           <div className="main-header__actions">
             <Search />
-            <FavoriteIcon className="favorite-icon" />
+            <div className="favorite-icon">
+              <FavoriteIcon className="icon" />
+              <span className="counter">{favoriteArticlesCount}</span>
+            </div>
           </div>
         </div>
       </Container>
