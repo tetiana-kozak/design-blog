50cb4012b501efa15416a6d87168506c68bf1764
Tetiana <kozaktanya077@gmail.com>
2023-04-08 23:23:56 +0300

    fix: add key

--- src/pages/ArticlePage/ArticlePage.tsx
+++ src/pages/ArticlePage/ArticlePage.tsx
@@ -8,6 +8,11 @@ import 'swiper/css/pagination'
 import author from 'assets/authors/leo-lei.jpg'
 import ArticlesArray, { Article, getArticlesObject } from 'utils/articlesArray'
 import { useParams } from 'react-router-dom'
+import { IconButton } from '@mui/material'
+import { useAppDispatch, useAppSelector } from 'redux/hooks'
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder'
+import FavoriteIcon from '@mui/icons-material/Favorite'
+import { toggleLike } from 'redux/favoritesReducer'
 
 type Props = {
   articlesObject?: {
@@ -21,6 +26,10 @@ const ArticlePage = ({
   const { id } = useParams()
 
   const article = articlesObject[parseInt(id!)]
+  const isFavorite = useAppSelector(
+    (state) => state.favoriteArticles[article.id]
+  )
+  const dispatch = useAppDispatch()
 
   return (
     <div className="single-article">
@@ -38,6 +47,13 @@ const ArticlePage = ({
         <div className="date-and-author">
           {article.date} <span> By {article.author}</span>
         </div>
+        <IconButton
+          disableRipple={true}
+          className="favorite-icon"
+          onClick={() => dispatch(toggleLike(article.id))}
+        >
+          {isFavorite ? <FavoriteIcon /> : <FavoriteBorderIcon />}
+        </IconButton>
       </section>
       <section className="article-slider">
         <Swiper
@@ -54,8 +70,8 @@ const ArticlePage = ({
           modules={[Pagination, Autoplay]}
           slidesPerView={1}
         >
-          {article.images?.map((sliderImage) => (
-            <SwiperSlide>
+          {article.images?.map((sliderImage, i) => (
+            <SwiperSlide key={i}>
               <div className="slider-slide">
                 <img src={sliderImage} alt="" className="slider-img" />
               </div>
@@ -66,11 +82,11 @@ const ArticlePage = ({
       <section className="article-body">
         <p>{article.content![0]}</p>
         <img src={article.images![0]} alt="" />
-        {article.content?.slice(1).map((paragraph) => (
-          <p>{paragraph}</p>
+        {article.content?.slice(1).map((paragraph, i) => (
+          <p key={i}>{paragraph}</p>
         ))}
-        {article.images?.slice(1).map((articleImage) => (
-          <img src={articleImage} alt="" />
+        {article.images?.slice(1).map((articleImage, i) => (
+          <img src={articleImage} alt="" key={i} />
         ))}
         <div className="photographer">
           Photos by <span> {article.photographer} </span>
