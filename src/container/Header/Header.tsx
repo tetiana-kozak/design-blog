0b6dddb54273fb75742c87adfa3b639d2ec308ac
Tetiana <kozaktanya077@gmail.com>
2023-04-08 18:36:52 +0300

    add favorites button to header

--- src/container/Header/Header.tsx
+++ src/container/Header/Header.tsx
@@ -3,16 +3,20 @@ import Menu from 'components/Menu/Menu'
 import Logo from 'components/Logo/Logo'
 import Search from 'components/Search/Search'
 import './Header.scss'
+import FavoriteIcon from '@mui/icons-material/Favorite'
 
 type Props = {}
 const Header = (props: Props) => {
   return (
     <header className="main-header">
       <Container>
-        <div className="main-header-inner">
+        <div className="main-header__inner">
           <Menu />
           <Logo />
-          <Search />
+          <div className="main-header__actions">
+            <Search />
+            <FavoriteIcon className="favorite-icon" />
+          </div>
         </div>
       </Container>
     </header>
