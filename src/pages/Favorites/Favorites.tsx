1fc4b61e3106345f47bf6c9e8e129a65a632f9bc
Tetiana <kozaktanya077@gmail.com>
2023-04-10 23:26:56 +0300

    add hover on favorite cards

--- src/pages/Favorites/Favorites.tsx
+++ src/pages/Favorites/Favorites.tsx
@@ -3,6 +3,7 @@ import { useAppSelector } from 'redux/hooks'
 import ArticlesArray, { getArticlesObject } from 'utils/articlesArray'
 import './Favorites.scss'
 import SectionTitle from 'components/Sections/SectionTitle'
+import { Link } from 'react-router-dom'
 
 type Props = {}
 const Favorites = (props: Props) => {
@@ -17,21 +18,23 @@ const Favorites = (props: Props) => {
       <SectionTitle sectionTitle="Favorite articles" />
       <Grid container spacing={6}>
         {filteredArray.map((article) => (
-          <Grid item sm={12} md={6} lg={4}>
-            <Card className="favorites__card">
-              <CardMedia
-                component="img"
-                image={article.mainImage}
-                alt={article.title}
-                className="favorites__img"
-              />
-              <CardContent className="favorites__card-content">
-                <div className="favorites__title">{article.title}</div>
-                <div className="favorites__description">
-                  {article.description}..
-                </div>
-              </CardContent>
-            </Card>
+          <Grid item sm={12} md={6} lg={4} key={article.id}>
+            <Link to={`/blog/${article.id}`}>
+              <Card className="favorites__card">
+                <CardMedia
+                  component="img"
+                  image={article.mainImage}
+                  alt={article.title}
+                  className="favorites__img"
+                />
+                <CardContent className="favorites__card-content">
+                  <div className="favorites__title">{article.title}</div>
+                  <div className="favorites__description">
+                    {article.description}..
+                  </div>
+                </CardContent>
+              </Card>
+            </Link>
           </Grid>
         ))}
       </Grid>
