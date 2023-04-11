511841a8c82f79e6011934ea2b5ba42aac986781
Tetiana <kozaktanya077@gmail.com>
2023-04-11 23:44:45 +0300

    add: no favorites img

--- src/pages/Favorites/Favorites.tsx
+++ src/pages/Favorites/Favorites.tsx
@@ -1,11 +1,13 @@
 import { Card, CardContent, CardMedia, Grid } from '@mui/material'
-import { useAppSelector } from 'redux/hooks'
+import { useAppDispatch, useAppSelector } from 'redux/hooks'
 import ArticlesArray, { getArticlesObject } from 'utils/articlesArray'
 import './Favorites.scss'
 import SectionTitle from 'components/Sections/SectionTitle'
 import { Link } from 'react-router-dom'
 import ClearIcon from '@mui/icons-material/Clear'
 import DeleteForeverIcon from '@mui/icons-material/DeleteForever'
+import { toggleLike } from 'redux/favoritesReducer'
+import noFavoritesImg from '../../assets/no-favorites.png'
 
 type Props = {}
 const Favorites = (props: Props) => {
@@ -15,33 +17,49 @@ const Favorites = (props: Props) => {
     (article) => favoritesState[article.id]
   )
 
+  const dispatch = useAppDispatch()
+
   return (
     <>
       <SectionTitle sectionTitle="Favorite articles" />
       <Grid container spacing={6}>
-        {filteredArray.map((article) => (
-          <Grid item sm={12} md={6} lg={4} key={article.id}>
-            <Link to={`/blog/${article.id}`}>
+        {filteredArray.length === 0 ? (
+          <Grid item sm={12} md={12} lg={12}>
+            <div className="no-favorites-block">
+              <p className="no-description">
+                You don't have any favorite articles yet
+              </p>
+              <img src={noFavoritesImg} alt="" className="no-image" />
+            </div>
+          </Grid>
+        ) : (
+          filteredArray.map((article) => (
+            <Grid item sm={12} md={6} lg={4} key={article.id}>
               <Card className="favorites__card">
-                <CardMedia
-                  component="img"
-                  image={article.mainImage}
-                  alt={article.title}
-                  className="favorites__img"
-                />
-                <CardContent className="favorites__card-content">
-                  <div className="favorites__title">{article.title}</div>
-                  <div className="favorites__description">
-                    {article.description}..
-                  </div>
-                </CardContent>
-                <div className="clear-icon">
+                <Link to={`/blog/${article.id}`}>
+                  <CardMedia
+                    component="img"
+                    image={article.mainImage}
+                    alt={article.title}
+                    className="favorites__img"
+                  />
+                  <CardContent className="favorites__card-content">
+                    <div className="favorites__title">{article.title}</div>
+                    <div className="favorites__description">
+                      {article.description}..
+                    </div>
+                  </CardContent>
+                </Link>
+                <div
+                  className="clear-icon"
+                  onClick={() => dispatch(toggleLike(article.id))}
+                >
                   <DeleteForeverIcon className="icon" /> <span>remove</span>
                 </div>
               </Card>
-            </Link>
-          </Grid>
-        ))}
+            </Grid>
+          ))
+        )}
       </Grid>
     </>
   )
