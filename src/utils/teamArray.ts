280c28d0e9108b0716392922cf38e64e2faaf37b
Tetiana <kozaktanya077@gmail.com>
2023-04-19 17:54:49 +0300

    add teamArray

---
+++ src/utils/teamArray.ts
@@ -0,0 +1,81 @@
+export type Team = {
+  name: string
+  position: string
+  photo: string
+  description: string
+}
+
+const TeamArray: Team[] = [
+  {
+    name: 'David Behringer',
+    position: 'Contributor',
+    photo: '/images/authors/David-Behringer.jpg',
+    description:
+      'Straying from his degree in business economics, David spent a summer in Italy to study art and language, only to embrace his love of design (and newfound love for espressos) when he returned home to California. His interests revolve around discovering the latest movers and shakers in the community, finding accessible design, and implementing ways to live well and with intention.',
+  },
+  {
+    name: 'Katie Treggiden',
+    position: 'Editor at Large',
+    photo: '/images/authors/Katie-Treggiden.jpg',
+    description:
+      'As a purpose-driven craft and design writer with almost 20 years experience in the creative industries, she also contributes to publications such as Viewpoint, The Guardian, Crafts Magazine, Elle Decoration, and Monocle24. She has recently completed a Masters in the History of Design at the University of Oxford, graduating with a distinction, and is a Fellow of the Royal Society of Arts and a Trustee of the Leach Pottery.',
+  },
+  {
+    name: 'Caroline Williamson',
+    position: 'Editor-in-Chief',
+    photo: '/images/authors/Caroline-Williamson.jpg',
+    description:
+      'When 7-year-old Caroline made it her mission to redecorate her room weekly, build endless LEGO villages, and repeatedly paint and reconstruct Barbies dream house, she was either obsessive or a young design enthusiast in the making. After getting her BFA in photography from SCAD, she moved to Los Angeles and worked for a glass artist + designer, which gave her insight into all aspects of the design world.',
+  },
+  {
+    name: 'Jaime Derringer',
+    position: 'Contributor',
+    photo: '/images/authors/Jaime-Derringer.jpg',
+    description:
+      'Jaime Derringer is a sought-after food designer and creative consultant, celebrated for staging theatrical, interactive food + beverage experiences.  Her clients include some of the worlds most notable brands as well as New Yorks foremost cultural institutions. With a degree in set design, Jaime has dedicated the past 15 years to blending her passion for scenography with her culinary prowess, innovating both domains and becoming a leader in New Yorks food design + event industry.',
+  },
+  {
+    name: 'Gregory Han',
+    position: 'Tech Editor',
+    photo: '/images/authors/gregory-han.jpg',
+    description:
+      'A Los Angeles native, Gregorys interests fall upon the relationship between design, nature, and technology. His resume includes art director, toy designer, managing editor at Apartment Therapy, and design contributor at the New York Times Wirecutter, alongside appearing in other publications as diverse as Toms Guide, Vice Munchies, and Metropolis.',
+  },
+  {
+    name: 'Kelly Beall',
+    position: 'Senior Editor',
+    photo: '/images/authors/kelly-beall.jpg',
+    description:
+      'Kelly Beall is senior editor and art director at InDesign. The Pittsburgh-based graphic designer and writer has had a deep love of art and design for as long as she can remember, and enjoys sharing her finds with others When undistracted by great art and design, she can be found making a mess in the kitchen, consuming as much information as possible, or on the couch with her three pets. ',
+  },
+  {
+    name: 'Leo Lei',
+    position: 'Contributor',
+    photo: '/images/authors/leo-lei.jpg',
+    description:
+      'Leo Lei translates his passion for minimalism into his daily-updated blog Leibal. In addition, you can find uniquely designed minimalist objects and furniture at the Leibal Store. Leo visits over 200 galleries every month to uncover and share the most exciting contemporary art in New York today.',
+  },
+  {
+    name: 'Dixie Duncan',
+    position: 'Director of Strategic Partnerships',
+    photo: '/images/authors/Dixie-Duncan.jpg',
+    description:
+      'A Jack and Jill magazine feature about her Montana childhood growing up with a bear was Dixie’s intro to media. Currently based in Seattle, her experience includes USA TODAY, GRAY Media, and the launch of an award-winning wedding magazine. She’s a non-stop networker, multi-tasker and closet voice-over artist who cherishes every moment spent with family and friends.',
+  },
+  {
+    name: 'Meredith Barberich',
+    position: 'Account Manager',
+    photo: '/images/authors/Meredith-Barberich.jpg',
+    description:
+      'Meredith is passionate about using design as a force for better living and the greater good. She has spent her career in global sales, marketing, and as a brand storyteller, including ten years as Brand Director at Dwell Magazine and teaching History of Industrial Design at the Academy of Art University in San Francisco. When she’s not collaborating with clients, Meredith can be found traveling with her family and noticing every architectural and design detail wherever she is – and then researching its history.',
+  },
+  {
+    name: 'Stephanie V. Jones',
+    position: 'Interim Chief Operating Officer',
+    photo: '/images/authors/Stephanie.jpg',
+    description:
+      'A native of Ohio, Stephanie graduated from The Ohio State University and loves living in Columbus. A CPA, Wife and Mom, she loves simplicity, minimalism, structure, and balance (especially when debits = credits). You can find her chasing her husband on a 3-mile run, visiting her daughter who is a US Marine, or at her son’s tennis tournaments. She balances by retreating to her “she cave” in the basement to exercise. When she’s not number crunching, you might also find her trying a new recipe in her kitchen with her poodle by her side to taste test.',
+  },
+]
+
+export default TeamArray
