17c020d1c3214e7540226f5a08613a11fc4023cc
Tetiana <kozaktanya077@gmail.com>
2023-04-21 12:26:32 +0300

    add media to components

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,6 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './container/App/App'
 import './reset.css'
 import './style.scss'
-import './media-style.scss'
 import { Provider } from 'react-redux'
 import { store } from 'redux/store'
 
