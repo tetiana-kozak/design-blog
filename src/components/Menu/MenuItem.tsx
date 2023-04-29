f85d6bec34f4e010d647bfd884a9fcb33c6c9070
Tetiana <kozaktanya077@gmail.com>
2023-04-29 23:38:02 +0300

    fix: 'all' category by default

--- src/components/Menu/MenuItem.tsx
+++ src/components/Menu/MenuItem.tsx
@@ -1,14 +1,21 @@
 import { NavLink } from 'react-router-dom'
 import './MenuItem.scss'
+import { useAppDispatch } from 'redux/hooks'
+import { selectedCategoryState } from 'redux/selectedCategoryReducer'
 
 type Props = {
   to: string
   children: React.ReactNode
 }
 const MenuItem = ({ to, children }: Props) => {
+  const dispatch = useAppDispatch()
   return (
     <li>
-      <NavLink to={to} className="menu-item">
+      <NavLink
+        to={to}
+        className="menu-item"
+        onClick={() => dispatch(selectedCategoryState('all'))}
+      >
         {children}{' '}
       </NavLink>
     </li>
