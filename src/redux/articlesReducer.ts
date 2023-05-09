c56e2d8e56ed8973fde3b9c63ce22d0f69740ef9
Tetiana <kozaktanya077@gmail.com>
2023-05-09 23:21:42 +0300

    add: all articles in json

--- src/redux/articlesReducer.ts
+++ src/redux/articlesReducer.ts
@@ -21,7 +21,8 @@ export const fetchArticles = createAsyncThunk<Article[], undefined>(
   'articles/fetchArticles',
   async () =>
     await axios
-      .get('https://run.mocky.io/v3/236dbed9-38a6-417d-a40e-5c799ad36c2b')
+      .get('https://run.mocky.io/v3/ce8ba2f6-ba0a-494c-9b9a-1135b74dfd80')
+      // .get('https://run.mocky.io/v3/236dbed9-38a6-417d-a40e-5c799ad36c2b')
       .then((response) => response.data.articles)
       .catch((error) => console.log('error: ', error))
 )
